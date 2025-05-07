f0d5f9b59296100da1d2efc089b40aa02d50318e
wepo <alex.mourot@icloud.com>
2025-05-07 22:01:55 +0200

    feat: modified init script to account for added provider for sui"

--- docker/common/post_node_init.sh
+++ docker/common/post_node_init.sh
@@ -70,19 +70,23 @@ cd /lava/specs/mainnet-1/specs/
 # specs=$(get_base_specs)
 lavad tx gov submit-legacy-proposal spec-add ./ibc.json,./tendermint.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 )
+sleep 5
 vote_yes_on_all_pending_proposals
 
 
 cd /lava/specs/mainnet-1/specs/
 lavad tx gov submit-legacy-proposal spec-add ./cosmossdk.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+sleep 2
 vote_yes_on_all_pending_proposals
 
 cd /lava/specs/mainnet-1/specs/
 lavad tx gov submit-legacy-proposal spec-add ./lava.json,./sui.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+sleep 2
 vote_yes_on_all_pending_proposals
 
 echo "Adding plan: DefaultPlan"
 lavad tx gov submit-legacy-proposal plans-add /lava/cookbook/plans/test_plans/default.json -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+sleep 2
 vote_yes_on_all_pending_proposals
 
 echo "Buying plan: DefaultPlan for $FROM"
@@ -93,15 +97,20 @@ echo "Staking provider"
 PROVIDERSTAKE="500000000000ulava"
 
 # Stake SUI JSON-RPC provider on port 80
-echo "Staking SUI JSON-RPC provider"
+echo "Staking SUI JSON-RPC provider1"
 SUI_PROVIDER_ADDRESS="nginx:80"
-lavad tx pairing stake-provider SUIJSONRPC $PROVIDERSTAKE "$SUI_PROVIDER_ADDRESS,1" 1 $(operator_address) -y --delegate-commission 50 --from servicer1 --provider-moniker "servicer1-sui" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+lavad tx pairing stake-provider SUIJSONRPC $PROVIDERSTAKE "$SUI_PROVIDER_ADDRESS,2" 2 $(operator_address) -y --delegate-commission 50 --from servicer1 --provider-moniker "servicer1-sui" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
-sleep 4
+sleep 5
 # Stake LAV1 provider on port 81 (this will cover all LAV1 API interfaces)
 echo "Staking LAV1 provider"
 LAV_PROVIDER_ADDRESS="nginx:81"
 lavad tx pairing stake-provider LAV1 $PROVIDERSTAKE "$LAV_PROVIDER_ADDRESS,1" 1 $(operator_address) -y --delegate-commission 50 --from servicer2 --provider-moniker "servicer2-lav" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
+
+sleep 5
+echo "Staking SUI JSON-RPC provider2"
+SUI_PROVIDER_ADDRESS="nginx:82"
+lavad tx pairing stake-provider SUIJSONRPC $PROVIDERSTAKE "$SUI_PROVIDER_ADDRESS,1" 1 $(operator_address) -y --delegate-commission 50 --from servicer2 --provider-moniker "servicer2-sui" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
 
 echo "### Post node init finished successfully ###"
