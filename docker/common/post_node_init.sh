7a4fa87586a93ff6648cb2252f55fa872be55040
wepo <alex.mourot@icloud.com>
2025-05-21 23:14:47 +0200

    feat: updated scripts for post node init

--- docker/common/post_node_init.sh
+++ docker/common/post_node_init.sh
@@ -10,6 +10,12 @@ check_votes_status() {
   lavad q gov proposals --output json | jq -r '.proposals[] | select(.status == "PROPOSAL_STATUS_VOTING_PERIOD")' 
 }
 
+# Function to get the current account sequence
+get_account_sequence() {
+  local account=$1
+  lavad query account $(lavad keys show $account -a) -o json | jq -r '.sequence'
+}
+
 # get_base_specs() {
 #     local priority_specs=(
 #         "specs/mainnet-1/specs/ibc.json"
@@ -70,46 +76,49 @@ cd /lava/specs/mainnet-1/specs/
 # specs=$(get_base_specs)
 lavad tx gov submit-legacy-proposal spec-add ./ibc.json,./tendermint.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 )
-sleep 5
+sleep 8
 vote_yes_on_all_pending_proposals
 
 
 cd /lava/specs/mainnet-1/specs/
 lavad tx gov submit-legacy-proposal spec-add ./cosmossdk.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
-sleep 2
+sleep 5
 vote_yes_on_all_pending_proposals
 
 cd /lava/specs/mainnet-1/specs/
 lavad tx gov submit-legacy-proposal spec-add ./lava.json,./sui.json --lava-dev-test -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
-sleep 2
+sleep 5
 vote_yes_on_all_pending_proposals
 
 echo "Adding plan: DefaultPlan"
 lavad tx gov submit-legacy-proposal plans-add /lava/cookbook/plans/test_plans/default.json -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
-sleep 2
+sleep 5
 vote_yes_on_all_pending_proposals
 
 echo "Buying plan: DefaultPlan for $FROM"
 lavad tx subscription buy DefaultPlan $(lavad keys show $FROM -a) --enable-auto-renewal -y --from $FROM --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE 2> /dev/null
 
-sleep 4
+sleep 20
 echo "Staking provider"
 PROVIDERSTAKE="500000000000ulava"
 
 # Stake SUI JSON-RPC provider on port 80
 echo "Staking SUI JSON-RPC provider1"
 SUI_PROVIDER_ADDRESS="nginx:80"
+echo "Current sequence for servicer1: $sequence"
 lavad tx pairing stake-provider SUIJSONRPC $PROVIDERSTAKE "$SUI_PROVIDER_ADDRESS,2" 2 $(operator_address) -y --delegate-commission 50 --from servicer1 --provider-moniker "servicer1-sui" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
-sleep 5
+sleep 20
 # Stake LAV1 provider on port 81 (this will cover all LAV1 API interfaces)
 echo "Staking LAV1 provider"
 LAV_PROVIDER_ADDRESS="nginx:81"
+echo "Current sequence for servicer2: $sequence"
 lavad tx pairing stake-provider LAV1 $PROVIDERSTAKE "$LAV_PROVIDER_ADDRESS,1" 1 $(operator_address) -y --delegate-commission 50 --from servicer2 --provider-moniker "servicer2-lav" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
-sleep 5
+sleep 20
 echo "Staking SUI JSON-RPC provider2"
 SUI_PROVIDER_ADDRESS="nginx:82"
+echo "Current sequence for servicer2: $sequence"
 lavad tx pairing stake-provider SUIJSONRPC $PROVIDERSTAKE "$SUI_PROVIDER_ADDRESS,1" 1 $(operator_address) -y --delegate-commission 50 --from servicer2 --provider-moniker "servicer2-sui" --gas-adjustment "1.5" --gas "auto" --gas-prices $GASPRICE
 
 
